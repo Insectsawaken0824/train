784490296ef292d07335ef4dc12dca191520813e
zhao <824718>
2017-10-17 15:37:09 +0800

    导入数据

--- Database/query.sql
+++ Database/query.sql
@@ -1,2 +1,4 @@
 SHOW DATABASES ;
 show variables like'%character%';
+# 16次运行跨3天 213次跨2天 1663次跨1天 9032次当天
+SELECT count(DISTINCT(train_number)) FROM train.train_number_stations_info WHERE other_day = 0 ;
