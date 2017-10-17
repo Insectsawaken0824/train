5c52732535336fd30a7b240c6ad3342572f164f5
zhao <824718>
2017-10-17 18:44:39 +0800

    导入数据

--- Database/query.sql
+++ Database/query.sql
@@ -1,4 +1,5 @@
 SHOW DATABASES ;
 show variables like'%character%';
-# 16次运行跨3天 213次跨2天 1663次跨1天 9032次当天
-SELECT count(DISTINCT(train_number)) FROM train.train_number_stations_info WHERE other_day = 0 ;
+# 16次运行跨3天 213次跨2天 1647次跨1天 8819次当天
+SELECT count(DISTINCT(train_number)) FROM train.train_number_stations_info ;
+SELECT * FROM formal_train_number_stations_info ORDER BY total_distance DESC LIMIT 20;
--- Database/table.sql
+++ Database/table.sql
@@ -31,3 +31,38 @@ CREATE TABLE train.train_number_stations_info(
   bak3 VARCHAR(255)
 )CHARACTER SET utf8 COLLATE utf8_general_ci;
 truncate table train.train_number_stations_info;
+
+# 车次停靠站信息表正式库
+DROP TABLE train.formal_train_number_stations_info;
+CREATE TABLE train.formal_train_number_stations_info(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  train_number VARCHAR(20) NOT NULL COMMENT '车次' ,
+  sequence_number INT COMMENT '到站序列号' ,
+  stations_name VARCHAR(20) NOT NULL COMMENT '站名',
+  distance INT COMMENT '距离' ,
+  total_distance INT COMMENT '总距离' ,
+  arrive_time VARCHAR(20) NOT NULL COMMENT '到达时间' ,
+  leave_time VARCHAR(20) NOT NULL COMMENT '离开时间' ,
+  runtime LONG NOT NULL COMMENT '行驶时间' ,
+  total_runtime LONG NOT NULL COMMENT '总行驶时间' ,
+  other_day INT COMMENT '0 当天 1 第二天 ..' ,
+  bak1 VARCHAR(255),
+  bak2 VARCHAR(255),
+  bak3 VARCHAR(255)
+)CHARACTER SET utf8 COLLATE utf8_general_ci;
+truncate table train.formal_train_number_stations_info;
+INSERT INTO train.formal_train_number_stations_info (train_number, sequence_number, stations_name, distance, total_distance, arrive_time, leave_time, runtime, total_runtime, other_day)
+  SELECT
+    train_number,
+    sequence_number,
+    stations_name,
+    distance,
+    total_distance,
+    arrive_time,
+    leave_time,
+    runtime,
+    total_runtime,
+    other_day
+  FROM train.train_number_stations_info
+  GROUP BY train_number, sequence_number, stations_name, distance, total_distance, arrive_time, leave_time, runtime,
+    total_runtime, other_day;
